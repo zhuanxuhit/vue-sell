3519557f044631080bb00a506de8bd932580fe6d
wangchao25 <wangchao25@baidu.com>
2018-03-19 14:15:41 +0800

    tab flex布局

--- src/main.js
+++ src/main.js
@@ -1,6 +1,10 @@
 import Vue from 'vue'
 import App from './App'
 import router from './router'
+import 'common/stylus/index.styl'
+import fastclick from 'fastclick'
+
+fastclick.attach(document.body)
 
 Vue.config.productionTip = false
 
